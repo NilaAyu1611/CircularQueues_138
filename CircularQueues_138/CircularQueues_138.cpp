86dd13e8c3afb44dac46834346eecad8a7a77ab8
NilaAyu1611 <nilaayucw@gmail.com>
2023-06-22 15:27:26 +0700

    method ini untuk mengcek apkaah antrian ini kosng atau tidak  jika FRONT nya itu di indes -1 maka masih kosong dan tampilkan

--- CircularQueues_138/CircularQueues_138.cpp
+++ CircularQueues_138/CircularQueues_138.cpp
@@ -38,6 +38,13 @@ public:
 		queue_array[REAR] = num;
 	}
 
-	
-	}
+	void remove() {
+		// Cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue underflow\n";
+			return;
+		}
+		cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+		
 };
