e66311a10c73a7c6f41cf272042401e19ccc7b70
NilaAyu1611 <nilaayucw@gmail.com>
2023-06-22 15:58:07 +0700

    jika fornt itu kurang dari saa dengan rear, amaka kana iterasi dari fornt awal- akhir

--- CircularQueues_138/CircularQueues_138.cpp
+++ CircularQueues_138/CircularQueues_138.cpp
@@ -70,4 +70,14 @@ public:
 			return;
 		}
 
+		cout << "\nElement in the queue are...\n";
+
+		// Jika FRONT_position <= REAR_position , iterasi dari FRONT hingga akhir array
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+			cout << endl;
+		}
 		
